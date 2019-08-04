ce87c160040171125e31dfd8c54c20e8dfa3b10d
Milen Khanal <milenkhanal@gmail.com>
2019-08-04 12:59:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ puts addition
 end
 
 def subtraction(num1, num2)
-
+return num1 - num2
+puts subtraction
 end
 
 def division(num1, num2)
-
+return num1 / num2 
+puts divison
 end
 
 def multiplication(num1, num2)
-
+return num1 * num2
+puts multiplication
 end
 
 def modulo(num1, num2)
-
+return num1 % num2
+puts modulo
 end
 
 def square_root(num)
+  
 
 end
